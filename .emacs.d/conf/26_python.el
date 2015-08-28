f87dd4bf63d72714e305ca8c4da97161d699318c
Takanori Hayashi <flowlight0@gmail.com>
2015-08-28 07:27:08 +0000

    Goに関する設定を追加

--- .emacs.d/conf/26_python.el
+++ .emacs.d/conf/26_python.el
@@ -1,10 +1,10 @@
-(el-get-bundle jedi)
-(el-get-bundle rope)
-(el-get-bundle elpy)
-(el-get-bundle py-autopep8)
-(el-get-bundle python-mode)
-(el-get-bundle pyflakes)
+;; (el-get-bundle jedi)
+;; (el-get-bundle rope)
+;; (el-get-bundle elpy)
+;; (el-get-bundle py-autopep8)
+;; (el-get-bundle python-mode)
+;; (el-get-bundle pyflakes)
 
-(add-hook 'before-save-hook 'py-autopep8-before-save)
-(elpy-enable)
+;; (add-hook 'before-save-hook 'py-autopep8-before-save)
+;; (elpy-enable)
 
---
+++ .emacs.d/conf/27_go.el
@@ -0,0 +1,6 @@
+(el-get-bundle go-mode)
+(el-get-bundle go-eldoc)
+(el-get-bundle go-autocomplete)
+(el-get-bundle go-def)
+
+
